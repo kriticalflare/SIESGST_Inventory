e5a014e05cea8243526a2ce06332871a21589acc
42350771+kriticalflare@users.noreply.github.com <krithiksuvarna@gmail.com>
2019-07-03 14:51:21 +0530

    Fix user return dialog content refresh

--- app/src/main/java/com/example/inventory/Adapter/User_Home_Adapter.java
+++ app/src/main/java/com/example/inventory/Adapter/User_Home_Adapter.java
@@ -32,6 +32,7 @@ public class User_Home_Adapter extends RecyclerView.Adapter<User_Home_Adapter.Us
 
     ArrayList<ComponentModel>componentModel;
     GoogleSignInClient mGoogleSignInClient;
+    Dialog dialog;
 
     public User_Home_Adapter(ArrayList<ComponentModel> componentModel) {
         this.componentModel = componentModel;
@@ -39,30 +40,32 @@ public class User_Home_Adapter extends RecyclerView.Adapter<User_Home_Adapter.Us
 
     @NonNull
     @Override
-    public User_Home_Adapter.User_Home_ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public User_Home_ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
        View view  = LayoutInflater.from(parent.getContext()).inflate(R.layout.admin_home_recycler_card,parent,false);
-       User_Home_ViewHolder userViewHolder = new User_Home_ViewHolder(view);
-        return userViewHolder;
-    }
+       final User_Home_ViewHolder userViewHolder = new User_Home_ViewHolder(view);
 
-    @Override
-    public void onBindViewHolder(@NonNull final User_Home_Adapter.User_Home_ViewHolder holder, int position) {
+        dialog = new Dialog(userViewHolder.context);
+        dialog.setContentView(R.layout.user_return_dialog);
 
-        holder.Component.setText(componentModel.get(position).getComponent());
-        holder.Count.setText(String.valueOf(componentModel.get(position).getCount()));
-        holder.Card.setOnClickListener(new View.OnClickListener() {
+
+        userViewHolder.Card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                         .requestEmail()
                         .build();
-                mGoogleSignInClient = GoogleSignIn.getClient(holder.context, gso);
-                final GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(holder.context);
-                final Dialog dialog = new Dialog(holder.context);
-                dialog.setContentView(R.layout.user_return_dialog);
+                mGoogleSignInClient = GoogleSignIn.getClient(userViewHolder.context, gso);
+                final GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(userViewHolder.context);
+
+                TextView user_dialog_component = (TextView)dialog.findViewById(R.id.return_dialog_component);
+                TextView user_dialog_count = (TextView)dialog.findViewById(R.id.return_dialog_quantity);
+
+                user_dialog_component.setText(componentModel.get(userViewHolder.getLayoutPosition()).getComponent());
+                user_dialog_count.setText(String.valueOf(componentModel.get(userViewHolder.getLayoutPosition()).getCount()));
+
                 dialog.setTitle("Request");
-                dialog.show();
                 dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.show();
                 dialog.findViewById(R.id.return_dialog_submit).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -71,7 +74,7 @@ public class User_Home_Adapter extends RecyclerView.Adapter<User_Home_Adapter.Us
                         final String dateString = sdf.format(date);
                         DatabaseReference myref;
                         myref =  FirebaseDatabase.getInstance().getReference("Components").child("Admin")
-                                .child(holder.Component.getText().toString()).child("count");
+                                .child(userViewHolder.Component.getText().toString()).child("count");
                         myref.addValueEventListener(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -84,22 +87,31 @@ public class User_Home_Adapter extends RecyclerView.Adapter<User_Home_Adapter.Us
                         });
                         DatabaseReference submitref;
                         submitref = FirebaseDatabase.getInstance().getReference("Requests");
-                        submitref.child(dateString).child("component").setValue(holder.Component.getText().toString());
+                        submitref.child(dateString).child("component").setValue(userViewHolder.Component.getText().toString());
                         submitref.child(dateString).child("datetime").setValue(dateString);
-                        submitref.child(dateString).child("requestcount").setValue(holder.Count.getText().toString());
+                        submitref.child(dateString).child("requestcount").setValue(userViewHolder.Count.getText().toString());
                         submitref.child(dateString).child("requesttype").setValue("return");
                         submitref.child(dateString).child("uname").setValue(account.getDisplayName());
                         submitref.child(dateString).child("uemail").setValue(account.getEmail());
                         dialog.dismiss();
-                        Toast.makeText(holder.context,"Request Sent",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(userViewHolder.context,"Request Sent",Toast.LENGTH_SHORT).show();
                     }
                 });
 
 
-                
+
             }
         });
 
+
+        return userViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final User_Home_ViewHolder userViewHolder, int position) {
+
+        userViewHolder.Component.setText(componentModel.get(position).getComponent());
+        userViewHolder.Count.setText(String.valueOf(componentModel.get(position).getCount()));
 
     }
 
